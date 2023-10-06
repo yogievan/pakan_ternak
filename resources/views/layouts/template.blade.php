2d3964e2eeacffa57789265186b6701ce9fbe04d
yogievan <yogievan32@gmail.com>
2023-10-06 09:38:29 +0700

    template dashboard

--- resources/views/layouts/template.blade.php
+++ resources/views/layouts/template.blade.php
@@ -13,34 +13,179 @@
     <link rel="preconnect" href="https://fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
 
+    <!-- fontawesome css-->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"/>
+
+
 </head>
 <body class="font-sans bg-gray-100">
 
     <!-- Header -->
-    <header class="bg-blue-500 p-4 text-white">
-        <h1 class="text-2xl font-semibold">Dashboard</h1>
+    <header class="border-b shadow-md dark:bg-gray-800 dark:border-gray-700 p-4 text-white">
+        <div class="flex">
+            <a href="#/dashboard/peternak">
+                <div class="flex">
+                    <img class="w-8 h-10 mr-2 my-auto" src="../assets/img/UKDW.png" alt="logo">
+                    <h1 class="text-2xl font-semibold my-auto">
+                        Sistem Informasi Pakan Ternak
+                    </h1>
+                </div>
+            </a>
+            <p class="ml-auto mr-5 my-auto font-semibold">
+                <-- USERNAME -->
+            </p>
+            <div class="mr-10">
+                <div class="w-[50px]">
+                    <img src="https://www.creative-tim.com/learning-lab/tailwind-starter-kit/img/team-2-800x800.jpg" alt="..." class="shadow rounded-full max-w-full h-auto align-middle border-none" />
+                </div>
+            </div>
+            <div class="my-auto">
+                <a href="#logout">
+                    <button class="bg-transparent border-2 border-red-600 text-red-600 font-bold py-2 px-4 rounded hover:bg-red-600 hover:text-white">
+                        LOG OUT
+                    </button>
+                </a>
+            </div>
+        </div>
     </header>
 
-    <!-- Sidebar -->
-    <aside class="bg-blue-700 text-white w-64 min-h-screen p-4">
-        <nav>
-            <ul>
-                <li class="mb-2"><a href="#" class="block p-2 rounded hover:bg-blue-600">Home</a></li>
-                <li class="mb-2"><a href="#" class="block p-2 rounded hover:bg-blue-600">Dashboard</a></li>
-                <li class="mb-2"><a href="#" class="block p-2 rounded hover:bg-blue-600">Settings</a></li>
-                <!-- Add more navigation links as needed -->
-            </ul>
-        </nav>
-    </aside>
+    <div class="flex">
+        <!-- Sidebar -->
+        <aside class="dark:bg-gray-800 dark:border-gray-700 text-white w-64 min-h-screen p-4 ">
+            <nav>
+                <ul>
+                    <li class="mb-2">
+                        <a href="#/dashboard/peternak" class="block p-2 rounded hover:bg-gray-400 hover:text-gray-800">
+                            <i class="fa fa-house mr-3"></i> Dashboard
+                        </a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="#/form/order/pakan" class="block p-2 rounded hover:bg-gray-400 hover:text-gray-800">
+                            <i class="fa fa-truck-ramp-box mr-3"></i> Form Order Pakan
+                        </a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="#/data/peternak" class="block p-2 rounded hover:bg-gray-400 hover:text-gray-800">
+                            <i class="fa fa-database mr-3"></i> Data Peternak
+                        </a>
+                    </li>
+                    <li class="mb-2">
+                        <a href="#/stok/pakan" class="block p-2 rounded hover:bg-gray-400 hover:text-gray-800">
+                            <i class="fa fa-boxes-stacked mr-3"></i> Stok Pakan
+                        </a>
+                    </li>
+                    <!-- Add more navigation links as needed -->
+                </ul>
+            </nav>
+        </aside>
+    
+        <!-- Main Content -->
+        <main class="flex p-4">
+            <!-- Your content goes here -->
+            <div class="py-3 pl-5 pr-5 font-bold text-gray-800">
+                <p class="text-2xl font-normal my-auto mb-3"> Dashboard Peternak</p>
+                
+                <!-- Add your content components here -->
+                <div class="grid grid-cols-4 gap-4">
+                    {{-- card --}}
+                    <div class="bg-white rounded shadow p-3 w-[250px] border">
+                        <div class="grid grid-cols-2">
+                            <div class="w-[120px]">
+                                <p class="text-lg font-medium">Data Ternak Sapi</p>
+                                <hr class="my-2">
+                                <p class="text-sm font-normal">
+                                    keterangan
+                                </p>
+                            </div>
+                            <div class="ml-auto w-[80px]">
+                                <img class="w-[70px]" src="../assets/img/sapi.png" alt="icon_sapi">
+                            </div>
+                        </div>
+                    </div>
+                    {{-- card --}}
+                    <div class="bg-white rounded shadow p-3 w-[250px] border">
+                        <div class="grid grid-cols-2">
+                            <div class="w-[120px]">
+                                <p class="text-lg font-medium">Data Ternak Puyuh</p>
+                                <hr class="my-2">
+                                <p class="text-sm font-normal">
+                                    keterangan
+                                </p>
+                            </div>
+                            <div class="ml-auto w-[80px]">
+                                <img class="w-[70px]" src="../assets/img/puyuh.png" alt="icon_puyuh">
+                            </div>
+                        </div>
+                    </div>
+                    {{-- card --}}
+                    <div class="bg-white rounded shadow p-3 w-[250px] border">
+                        <div class="grid grid-cols-2">
+                            <div class="w-[120px]">
+                                <p class="text-lg font-medium">Data Ternak Ayam</p>
+                                <hr class="my-2">
+                                <p class="text-sm font-normal">
+                                    keterangan
+                                </p>
+                            </div>
+                            <div class="ml-auto w-[80px]">
+                                <img class="w-[70px]" src="../assets/img/ayam.png" alt="icon_ayam">
+                            </div>
+                        </div>
+                    </div>
+                    {{-- card --}}
+                    <div class="bg-white rounded shadow p-3 w-[250px] border">
+                        <div class="grid grid-cols-2">
+                            <div class="w-[120px]">
+                                <p class="text-lg font-medium">Data Ternak Kambing</p>
+                                <hr class="my-2">
+                                <p class="text-sm font-normal">
+                                    keterangan
+                                </p>
+                            </div>
+                            <div class="ml-auto w-[80px]">
+                                <img class="w-[70px]" src="../assets/img/kambing.png" alt="icon_kambing">
+                            </div>
+                        </div>
+                    </div>
+                </div>
 
-    <!-- Main Content -->
-    <main class="flex-1 p-4">
-        <!-- Your content goes here -->
-        <div class="bg-white p-4 rounded shadow">
-            <h2 class="text-xl font-semibold mb-4">Welcome to the Dashboard</h2>
-            <!-- Add your content components here -->
+                <div class="grid grid-cols-2 gap-4 mt-4">
+                    {{-- card --}}
+                    <div class="bg-white rounded shadow p-3 border">
+                        <div>
+                            <p class="text-lg font-bold">Data Silo Pakan Ternak</p>
+                            <hr class="my-2">
+                            <p class="text-sm font-normal">
+                                keterangan
+                            </p>
+                        </div>
+                    </div>
+                    {{-- card --}}
+                    <div class="bg-white rounded shadow p-3 border">
+                        <div>
+                            <p class="text-lg font-bold">Stok Tipis</p>
+                            <hr class="my-2">
+                            <p class="text-sm font-normal">
+                                keterangan
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </main>
+    </div>
+    <!-- Footer -->
+    @php
+        $date = date("l, d F Y");
+    @endphp
+    <footer class="border-t shadow-md dark:bg-gray-800 dark:border-gray-700 p-4 text-white">
+        <div class="flex gap-2">
+            <i class="fa-solid fa-circle-info my-auto"></i>
+            <p class="pl-3 text-white">
+                Dibuat oleh ASDOS RPL 2023
+                {{$date}}
+            </p>
         </div>
-    </main>
-
+    </footer>
 </body>
 </html>
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -18,8 +18,8 @@
         <section class="bg-gray-50 dark:bg-gray-900">
             <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                 <a href="#" class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-                    <img class="w-8 h-9 mr-2" src="../assets/img/UKDW.png" alt="logo">
-                    Pakan Ternak    
+                    <img class="w-8 h-10 mr-2" src="../assets/img/UKDW.png" alt="logo">
+                    Sistem Informasi Pakan Ternak    
                 </a>
                 <div class="w-full bg-white rounded-lg border dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                     <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
@@ -40,6 +40,6 @@
                     </div>
                 </div>
             </div>
-            </section>
+        </section>
     </body>
 </html>
